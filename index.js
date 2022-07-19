3877f510dc0809719c2009c7e66b7a5619c65a1b
redolf <redolf639@gmail.com>
2022-07-19 15:29:09 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var buttonColours = ["red","blue","green","yellow"];
+var randonChosenColor 
+
+function nextSequence () {
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber);
+}
